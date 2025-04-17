a198139c62ecf437164f29032410e0633f9c05a6
Charley <charleyvieyra143@gmail.com>
2025-04-17 02:54:01 -0700

    update

--- project1/Backend/database.py
+++ project1/Backend/database.py
@@ -66,4 +66,4 @@ def login():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # Create the database tables
-    app.run(host="0.0.0.0",debug = True, port=5001)
+    app.run(host="0.0.0.0",debug = True, port=8080)
